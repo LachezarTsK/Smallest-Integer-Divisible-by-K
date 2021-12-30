237cdc9c1567bd92353fc8fda332781b5de5373e
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-12-30 16:12:31 +0100

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+
+    public int smallestRepunitDivByK(int k) {
+
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
+
+        int remainder = 1;
+        int numberOfDigits = 1;
+
+        while (remainder % k != 0) {
+            remainder = (remainder * 10 + 1) % k;
+            if (numberOfDigits++ > k) {
+                return -1;
+            }
+        }
+        return numberOfDigits;
+    }
+}
